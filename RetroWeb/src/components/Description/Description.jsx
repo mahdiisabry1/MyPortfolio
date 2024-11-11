e52cdbfda064adc098be9d99c4597f69c145d11c
mahdiisabry1 <mahdiisabry@gmail.com>
2024-11-11 22:29:00 +0530

    remove loader

--- RetroWeb/src/components/Description/Description.jsx
+++ RetroWeb/src/components/Description/Description.jsx
@@ -157,7 +157,122 @@ const Description = () => {
         </div>
       </div>
       <Featured isFiltered={isFiltered} />
-      <div className="skill-container"></div>
+      <div className="skill-container">
+        <div className="techstackchoose">
+          <div className="brutalist-card">
+            <div className="brutalist-card__header">
+              <div className="brutalist-card__icon">
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path>
+                </svg>
+              </div>
+              <div className="brutalist-card__alert">Warning</div>
+            </div>
+            <div className="brutalist-card__message">
+              This is a brutalist card with a very angry button. Proceed with
+              caution, youve been warned.
+            </div>
+            <div className="brutalist-card__actions">
+              <a
+                className="brutalist-card__button brutalist-card__button--mark"
+                href="#"
+              >
+                Mark as Read
+              </a>
+              <a
+                className="brutalist-card__button brutalist-card__button--read"
+                href="#"
+              >
+                Okay
+              </a>
+            </div>
+          </div>
+          <div className="brutalist-card">
+            <div className="brutalist-card__header">
+              <div className="brutalist-card__icon">
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path>
+                </svg>
+              </div>
+              <div className="brutalist-card__alert">Warning</div>
+            </div>
+            <div className="brutalist-card__message">
+              This is a brutalist card with a very angry button. Proceed with
+              caution, youve been warned.
+            </div>
+            <div className="brutalist-card__actions">
+              <a
+                className="brutalist-card__button brutalist-card__button--mark"
+                href="#"
+              >
+                Mark as Read
+              </a>
+              <a
+                className="brutalist-card__button brutalist-card__button--read"
+                href="#"
+              >
+                Okay
+              </a>
+            </div>
+          </div>
+          <div className="brutalist-card">
+            <div className="brutalist-card__header">
+              <div className="brutalist-card__icon">
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path>
+                </svg>
+              </div>
+              <div className="brutalist-card__alert">Warning</div>
+            </div>
+            <div className="brutalist-card__message">
+              This is a brutalist card with a very angry button. Proceed with
+              caution, youve been warned.
+            </div>
+            <div className="brutalist-card__actions">
+              <a
+                className="brutalist-card__button brutalist-card__button--mark"
+                href="#"
+              >
+                Mark as Read
+              </a>
+              <a
+                className="brutalist-card__button brutalist-card__button--read"
+                href="#"
+              >
+                Okay
+              </a>
+            </div>
+          </div>
+          <div className="brutalist-card">
+            <div className="brutalist-card__header">
+              <div className="brutalist-card__icon">
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path>
+                </svg>
+              </div>
+              <div className="brutalist-card__alert">Warning</div>
+            </div>
+            <div className="brutalist-card__message">
+              This is a brutalist card with a very angry button. Proceed with
+              caution, youve been warned.
+            </div>
+            <div className="brutalist-card__actions">
+              <a
+                className="brutalist-card__button brutalist-card__button--mark"
+                href="#"
+              >
+                Mark as Read
+              </a>
+              <a
+                className="brutalist-card__button brutalist-card__button--read"
+                href="#"
+              >
+                Okay
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- RetroWeb/src/pages/views/Home.jsx
+++ RetroWeb/src/pages/views/Home.jsx
@@ -3,7 +3,6 @@ import Description from "../../components/Description/Description";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
-import PreLoader from "../../components/PreLoader/PreLoader";
 import Status from "../../components/Status/Status";
 import Tag from "../../components/Tag/Tag";
 import "../styles/Home.css";
@@ -34,7 +33,6 @@ const Home = () => {
           top: `${position.y}px`,
         }}
       />
-      <PreLoader />
       <Navbar />
       <div className="displayMedia">
         <Tag />
