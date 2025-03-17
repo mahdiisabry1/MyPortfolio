820c3ac7a49a3d66bc727612b2b685e2c3033739
mahdiisabry1 <mahdiisabry@gmail.com>
2025-03-17 21:26:33 +0530

    Update App.jsx

--- RetroWeb/src/App.jsx
+++ RetroWeb/src/App.jsx
@@ -4,14 +4,14 @@ import { useEffect, useState } from "react";
 import Contact from "./pages/views/Contact.jsx";
 
 function App() {
-  // useEffect(() => {
-  //   const disableRightClick = (e) => e.preventDefault();
-  //   document.addEventListener("contextmenu", disableRightClick);
+  useEffect(() => {
+    const disableRightClick = (e) => e.preventDefault();
+    document.addEventListener("contextmenu", disableRightClick);
 
-  //   return () => {
-  //     document.removeEventListener("contextmenu", disableRightClick);
-  //   };
-  // }, []);
+    return () => {
+      document.removeEventListener("contextmenu", disableRightClick);
+    };
+  }, []);
 
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
