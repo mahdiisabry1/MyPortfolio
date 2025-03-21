cb0d03f630be5ba2a3ea1925c4e2e754fe0db327
mahdiisabry1 <mahdiisabry@gmail.com>
2025-03-21 23:19:23 +0530

    contact

--- RetroWeb/src/pages/views/Contact.jsx
+++ RetroWeb/src/pages/views/Contact.jsx
@@ -5,7 +5,19 @@ const Contact = () => {
   return (
     <>
       <Navbar />
-      <div className="">This is the Contact Page</div>
+      <div className="contact-container">
+        <div className="contact-header">
+          <h2>Contact Us</h2>
+        </div>
+        <form className="contact-form">
+          <input type="text" placeholder="Name" className="input-field" />
+          <input type="email" placeholder="Email" className="input-field" />
+          <textarea placeholder="Message" className="message-field"></textarea>
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
+      </div>
     </>
   );
 };
