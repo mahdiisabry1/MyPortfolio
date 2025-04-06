99a3360ca5083ad1b889325e635c052a54d53ba3
mahdiisabry1 <mahdiisabry@gmail.com>
2025-04-06 18:42:39 +0530

    Update Tag.jsx

--- RetroWeb/src/components/Tag/Tag.jsx
+++ RetroWeb/src/components/Tag/Tag.jsx
@@ -7,25 +7,25 @@ const Tag = () => {
         <h2>
           Got a project in mind? Let’s bring your vision to life! Whether it’s a
           new startup or an innovative idea, I’m here to help make it happen.
-          Feel free to <a href="mailto:mahdiisabry1@email.com">Mail</a> to
+          Feel free to <a href="mailto:mahdisabry.hello@gmail.com">Mail</a> to
           discuss how we can turn your ideas into reality.
         </h2>
         <h2>
           Got a project in mind? Let’s bring your vision to life! Whether it’s a
           new startup or an innovative idea, I’m here to help make it happen.
-          Feel free to <a href="mailto:mahdiisabry1@email.com">Mail</a> to
+          Feel free to <a href="mailto:mahdisabry.hello@gmail.com">Mail</a> to
           discuss how we can turn your ideas into reality.
         </h2>
         <h2>
           Got a project in mind? Let’s bring your vision to life! Whether it’s a
           new startup or an innovative idea, I’m here to help make it happen.
-          Feel free to <a href="mailto:mahdiisabry1@email.com">Mail</a> to
+          Feel free to <a href="mailto:mahdisabry.hello@gmail.com">Mail</a> to
           discuss how we can turn your ideas into reality.
         </h2>
         <h2>
           Got a project in mind? Let’s bring your vision to life! Whether it’s a
           new startup or an innovative idea, I’m here to help make it happen.
-          Feel free to <a href="mailto:mahdiisabry1@email.com">Mail</a> to
+          Feel free to <a href="mailto:mahdisabry.hello@gmail.com">Mail</a> to
           discuss how we can turn your ideas into reality.
         </h2>
       </div>
